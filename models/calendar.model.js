f9b58620939f8c031a45a04a41ede09d27a17d73
SebastianSamboni <sebas_18_08@hotmail.com>
2024-01-09 10:44:08 -0500

    Add relations in Calendar model

--- models/calendar.model.js
+++ models/calendar.model.js
@@ -1,5 +1,6 @@
 import { DataTypes } from 'sequelize'
 import { sequelize } from '../database/database.js'
+import { Task } from './task.model.js'
 
 export const Calendar = sequelize.define('calendars', {
     id: {
@@ -9,4 +10,13 @@ export const Calendar = sequelize.define('calendars', {
     }
 }, {
     timestamps: false
+})
+
+Calendar.hasMany(Task, {
+    foreignKey: 'calendar_id',
+    sourceKey: 'id'
+})
+Task.belongsTo(Calendar, {
+    foreignKey: 'calendar_id',
+    target: 'id'
 })
