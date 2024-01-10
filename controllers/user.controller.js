e1530a303c66def4bf7cd812f2534c13766d9191
SebastianSamboni <sebas_18_08@hotmail.com>
2024-01-10 00:22:18 -0500

    Add .vscode folder to gitignore

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,6 +4,18 @@ import bcrypt from 'bcryptjs'
 export const createAccount = async (req, res) => {
     const { name, last_name, nickname, email, password, cellphone } = req.body
     try {
+        if (!password || password.length < 8 || password.length > 20) {
+            return res.status(400).json({
+                error: 'La contraseña debe tener entre 8 y 20 caracteres!'
+            })
+        }
+        const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d).+$/
+        if (!passwordRegex.test(password)) {
+            return res.status(400).json({
+                error: 'La contraseña debe contener al menos una letra y un número'
+            })
+        }
+
         const pwdHash = await bcrypt.hash(password, 10)
         const newUser = User.create({
             name,
